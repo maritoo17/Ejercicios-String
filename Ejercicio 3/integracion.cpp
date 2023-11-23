d8a15b72cfc1c13aa0e8d38246f0618ac34759ac
maritoo17 <msancgim@myuax.com>
2023-11-23 12:28:51 +0100

    Ejercicio 3 en el que creo una cadena string con la suma de varias frases para dar luego una cadena unida con un sentido lógico.

---
+++ Ejercicio 3/integracion.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str1 = "Rubén";
+    string str2 = "ponme";
+    string str3 = "una";
+    string str4 = "buena";
+    string str5 = "nota";
+    string str6 = "porfavor";
+    string str7 = str1 + " " + str2 + " " + str3 + " " + str4 + " " + str5 + " " + str6;
+    cout << "Cadena resultante: " << str7 << endl;
+    return 0;
+}
